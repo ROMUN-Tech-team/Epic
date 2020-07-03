76e2394c6fd807f5ef17deb0f170411780708abc
HeLin Liu <musdatech@gmail.com>
2020-07-03 17:51:09 +0800

    Fix title

--- article.php
+++ article.php
@@ -5,8 +5,38 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
 <html lang="en">
+<?php
+require_once("template/header.php");
+
+session_start();
+include("inc/config.php");
+$id = $_REQUEST["id"];
+
+
+$result = mysqli_query($con, "select * from article WHERE id = '{$id}';");
+mysqli_query($con, "UPDATE article SET count = count + 1 WHERE id = '{$id}';");
+
+
+while ($row = mysqli_fetch_array($result)) {
+    $dbcount = $row["count"];
+    $dbtitle = $row["title"];
+    $dbsubtitle = $row["subtitle"];
+    $dbsummary = $row["summary"];
+    $dbarticle = $row["article"];
+    $dblang = $row["lang"];
+
+    if ($dblang == "CN") {
+        $dblang = "中文";
+    } elseif ($dblang == "EN") {
+        $dblang = "English";
+    };
+
+}
+
+
+?>
 <head>
-    <title>主页 - <?php require_once("template/title.php"); ?></title>
+    <title><?php echo $dbtitle ?> - <?php require_once("template/title.php"); ?></title>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
     <link rel="stylesheet" href="assets/css/main.css"/>
@@ -21,36 +51,7 @@
         <div class="inner">
 
             <!-- Header -->
-            <?php
-            require_once("template/header.php");
 
-            session_start();
-            include("inc/config.php");
-            $id = $_REQUEST["id"];
-
-
-            $result = mysqli_query($con, "select * from article WHERE id = '{$id}';");
-            mysqli_query($con, "UPDATE article SET count = count + 1 WHERE id = '{$id}';");
-
-
-            while ($row = mysqli_fetch_array($result)) {
-                $dbcount = $row["count"];
-                $dbtitle = $row["title"];
-                $dbsubtitle = $row["subtitle"];
-                $dbsummary = $row["summary"];
-                $dbarticle = $row["article"];
-                $dblang = $row["lang"];
-
-                if ($dblang == "CN") {
-                    $dblang = "中文";
-                } elseif ($dblang == "EN") {
-                    $dblang = "English";
-                };
-
-            }
-
-
-            ?>
 
             <section>
                 <header class="main">
