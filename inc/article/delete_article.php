31cc6b5b3e9ef2338c965723bd72273f8b0134ad
HeLin Liu <musdatech@gmail.com>
2020-07-13 22:15:23 +0800

    Delete article

--- inc/article/delete_article.php
+++ inc/article/delete_article.php
@@ -6,3 +6,13 @@
  * Email: <musdatech@gmail.com>
  *
  */
+
+session_start();
+
+$id = $_REQUEST["id"];
+
+include("../config.php");
+
+mysqli_query($con, "DELETE FROM article WHERE id = {$id}");
+
+echo 1;
