bbd4aaa0497cf0bd895368852f2f88f1d01a573b
horace <musdatech@gmail.com>
2020-07-31 23:35:31 +0800

    Support Aster System

--- inc/account/login.php
+++ inc/account/login.php
@@ -6,68 +6,91 @@
 // 0 = True
 
 session_start();
-$account = $_REQUEST["account"];
+
+$asterid = $_REQUEST["asterid"];
 $password = $_REQUEST["password"];
+$ip = $_SERVER["HTTP_X_REAL_IP"];
 
-include("../config.php");
+$api_key = "87sbt78an8A7T8S878TA8t9763889ts87TE78D3";
 
-if (!$con) {
-    die('数据库连接失败' . mysqli_error($con));
+function postAction($asterid, $password, $ip, $api_key)
+{
+
+    // 请求url
+    $url = "https://aster.romun.cn/inc/api/api-login.php";
+    // 参数数组
+    $parameters = array(
+        'asterid' => $asterid,
+        'password' => $password,
+        'ip' => $ip,
+        'api-key' => $api_key
+    );
+    // 初始化
+    $ch = curl_init();
+
+    // 设置变量
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $parameters);
+
+    // 执行
+    $return = curl_exec($ch);
+
+    // 关闭
+    curl_close($ch);
+
+    return $return;
+
 }
 
-$dbaccount = null;
-$dbpassword = null;
-$dbcom = null;
-$dbisdelete = null;
+if ($_SESSION[$ip] < 3) {
 
-public function PostAction($url , $data=array()){
+    $data = postAction($asterid, $password, $ip, $api_key);
 
-        $ch = curl_init();
+    $data = json_decode($data);
 
-        curl_setopt($ch, CURLOPT_URL, $url);
+    if ($data->status != "0") {
+        ini_set('session.gc_maxlifetime', 300); //设置SESSION过期时间
 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        if (!isset($_SESSION['ok'])) {
+            $_SESSION[$ip] = $_SESSION[$ip] + 1;//累积登录错误次数
+        }
 
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        $lifeTime = 1800;
+        setcookie(session_name(), session_id(), time() + $lifeTime, "/");
 
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
 
-        // POST数据
-
-        curl_setopt($ch, CURLOPT_POST, 1);
-
-        // 把post的变量加上
-
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-
-        $output = curl_exec($ch);
-
-        curl_close($ch);
-
-        return $output;
-
+        echo $data->status;
+        exit;
     }
 
-$result = PostAction("https://aster.romun.cn/inc/api/");
+    include("../config.php");
 
-if (is_null($dbaccount)) {
+    $result = mysqli_query($con, "select * from user where asterid = '{$asterid}'");
 
-    echo 2;
+    while ($row = mysqli_fetch_array($result)) {
+        $dbasterid = $row["asterid"];
+    }
 
-} else if ($dbisdelete == 1) {
 
-    echo 1;
-
-} elseif ($dbpassword != $password) {
-
-    echo 3;
+    if ($dbasterid == "") {
+        $_SESSION["asterid"] = $data->asterid;
+        $_SESSION["code"] = mt_rand(0, 1000000);
+        $_SESSION['ok'] = 1;
+        $_SESSION[$ip] = 0;
+        echo 58;
+    } else {
+        $_SESSION["asterid"] = $data->asterid;
+        $_SESSION["code"] = mt_rand(0, 1000000);
+        $_SESSION['ok'] = 1;
+        $_SESSION[$ip] = 0;
+        echo 60;
+    }
 
 } else {
-    $_SESSION["account"] = $dbaccount;
-    $_SESSION["com"] = $dbcom;
-    $_SESSION["isdelete"] = $dbisdelete;
-    $_SESSION["code"] = mt_rand(0, 100000);
-    mysqli_query($con, "update user set lastlogin=now() where account='{$account}'") or die ("存入数据库失败" . mysqli_error($con));
-
+    echo 5;
 }
+
 mysqli_close($con);
